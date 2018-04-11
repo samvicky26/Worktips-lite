2036e1c566c66903aa20933929e1f00ce26edf44
samvicky <billavignesh26@gmail.com>
2018-04-11 11:45:46 +0530

    fix

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -136,7 +136,7 @@ const uint32_t  P2P_IDLE_CONNECTION_KILL_INTERVAL            = (5*60);
 
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1510d13336228debfee9c918ce505efe225d8c94d045fa115";
 
-const char* const SEED_NODES[] = { "0.0.0.0:17239" };
+const char* const SEED_NODES[] = {  };
 
 
 struct CheckpointData {
